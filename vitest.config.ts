3984ae588d9c6d660f265aa3e8638bd4d261dfd4
mikoloism <mikoloism@gmail.com>
2023-08-28 15:57:35 +0330

    ci: initial projects

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import * as path from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	test: {
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json', 'html'],
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './packages'),
+		},
+	},
+});
