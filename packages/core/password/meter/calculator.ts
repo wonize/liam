2f40734bc20e0ecc1c48b780cc6b243429542d3b
mikoloism <mikoloism@gmail.com>
2023-09-11 15:36:53 +0330

    perf(core/pass/meter): improve PasswordMeterCalculator
    refactored with `getter` and `setter` members and  extended of responsibilities into methods

--- packages/core/password/meter/calculator.ts
+++ packages/core/password/meter/calculator.ts
@@ -1,16 +1,84 @@
 import { PasswordEntity } from '@/core/password/entity/password.entity';
 
+const ALPHABET_PATTERN = /-/gm;
+const ALPHABET_LOWERCASE_PATTERN = /-/gm;
+const ALPHABET_UPPERCASE_PATTERN = /-/gm;
+const DIGITS_PATTERN = /-/gm;
+const PASSWORD_MINIMUM_SIZE = 4;
+const MINIMUM_STRONG = 1;
+
 class PasswordMeterCalculator {
-	public constructor() {}
+	#_value: number = MINIMUM_STRONG;
 
-	public calculate(password: string | PasswordEntity) {
-		let value = password.valueOf();
+	set #value(value: boolean) {
+		const sign = value === true ? 1 : -1;
+		const nextValue = this.#value + sign;
+		this.#_value = Math.max(nextValue, MINIMUM_STRONG);
+	}
+	get #value(): number {
+		return this.#_value;
+	}
 
+	#password: string;
+	public constructor(password: string | PasswordEntity) {
+		this.#password = password.toString();
+	}
+
+	public hasAlphabet(): boolean {
+		return ALPHABET_PATTERN.test(this.#password);
+	}
+
+	public hasLowercase(): boolean {
+		return ALPHABET_LOWERCASE_PATTERN.test(this.#password);
+	}
+
+	public hasUppercase(): boolean {
+		return ALPHABET_UPPERCASE_PATTERN.test(this.#password);
+	}
+
+	public hasDigits(): boolean {
+		return DIGITS_PATTERN.test(this.#password);
+	}
+
+	public hasMinimumSize(): boolean {
+		return this.#password.length > PASSWORD_MINIMUM_SIZE;
+	}
+
+	#calcSize() {
+		const hasMinSize = this.hasMinimumSize();
+
+		if (hasMinSize) {
+			for (let index = 0; index < this.#password.length; index++) {
+				this.#value;
+			}
+		}
+
+		this.#value = hasMinSize;
+	}
+
+	#calcAlphabet() {
+		this.#value = this.hasAlphabet();
+	}
+
+	#calcLowercase() {
+		this.#value = this.hasLowercase();
+	}
+
+	#calcUppercase() {
+		this.#value = this.hasUppercase();
+	}
+
+	#calcDigits() {
+		this.#value = this.hasDigits();
+	}
+
+	public calculate() {
 		let score = 0;
 		let length = 0;
 		let specialChar = 0;
 		let caseMix = 0;
 		let numCharMix = 0;
+		const value = this.#password;
 
 		const specialCharRegex = /[^A-Za-z0-9]/g;
 		const lowercaseRegex = /(.*[a-z].*)/g;
